9ec7ad9c35a58cff75a49cbe963f81e055a0d2f7
Chris Corrigan <chriscorrigan14@gmail.com>
2018-07-15 19:57:19 -0700

    leeeetle fix

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-    const notes = require('.../controllers/note.controller.js');
+    const notes = require('../controllers/note.controller.js');
 
     // create new note
     app.post('/notes', notes.create);
